b3a94f5d18ac412ecdec16837a3ca5405c692b8f
Brent Stradling <brent.stradling@gmail.com>
2015-07-20 20:05:20 -0500

    Web Scrapper
    Started implementing the web scraper I made forever ago. Still has
    major issues, needs a lot of work.

--- Public/controllers/webscrapper.js
+++ Public/controllers/webscrapper.js
@@ -1,50 +1,49 @@
-    var app = angular.module('emailScrape', ['ngRoute']);
-myApp.controller('webscrapper', ['$scope', '$http', function($scope, $http){
+angular.module('scrape', ['ngRoute', 'ngCsvImport']);
 
-  var scrape = function(){
-    $http.get('/webscrapper').success(function(response){
-    console.log('I got the data I requested');
-    $scope.scrape = response;
+app.controller('MainCtrl', function ($scope) {
+    $scope.showContent = function($fileContent){
+        $scope.content = $fileContent;
+    };
   });
-};
-refresh();
-});
-//Web Scrapping function that I might get working someday
 
-//   $scope.scrape = function(){
-// url = $scope.client.url;
-//
-//     console.log($scope.client.url);
-//     request(url, function(error, response, html){
-//
-//   		if(!error){
-//
-//   			var title, name, link;
-//   			var json = { title : "", name : "", link : ""};
-//
-//   			$('.headline').filter(function(){
-//   		        var data = $(this);
-//   		        title = data.text();
-//   		        json.title = title;
-//               })
-//
-//   			$('.container').filter(function(){
-//   		        var data = $(this);
-//   		        name = data.find().last().text();
-//   		        json.name = name;
-//   		        json.link = link;
-//   	        })
-//   		}
-//
-//   		fs.appendFile('output.json', JSON.stringify(json, null, 4), function(err){
-//           	console.log('File successfully written! - Check your project directory for the output.json file');
-//           	console.log(title);
-//           	console.log(name);
-//           	console.log(url);
-//           })
-//
-//           res.send('<a href="'+ url +'">'+ title +'</a><br /><br />');
-//           fs.appendFile('emaillist.txt','<a href="'+ url +' target="_blank">'+ title +'</a>', function(err){});
-//
-//   	});
-// };
+app.directive('onReadFile', function ($parse) {
+	return {
+		restrict: 'A',
+		scope: false,
+		link: function(scope, element, attrs) {
+            var fn = $parse(attrs.onReadFile);
+
+			element.on('change', function(onChangeEvent) {
+				var reader = new FileReader();
+
+				reader.onload = function(onLoadEvent) {
+					scope.$apply(function() {
+						fn(scope, {$fileContent:onLoadEvent.target.result});
+					});
+				};
+
+				reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
+			});
+		}
+	};
+});
+
+app.controller('webscrapper', ['$scope', '$http', function($scope, $http){
+
+$scope.scrape = function(url) {
+  console.log("Found scrape");
+  $http.post('clientlist/', url).success(function(data){
+    $scope.url = data;
+    console.log("pushed url to list" + data);
+  });
+
+    $http.get('/scrape').success(function(data){
+      $scope.clientlist = scrape;
+      console.log("IT WORKED");
+      console.log(data);
+    })
+
+    };
+}
+
+]);
